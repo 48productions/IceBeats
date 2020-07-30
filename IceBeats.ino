b8e50ab6c712f3f4b4ec9a56818dc8f13dad171e
48productions <48productions@gmail.com>
2020-07-29 21:32:44 -0600

    IceBeats: Now with AGC and better beat detection!
      * Songs should visualize similarly regardless of the input's volume - hyper quiet songs no longer only turn on 1 LED, hyper loud songs no longer always max out all the LEDs
    
      * The threshold to detect a beat at now decreases if beats aren't seen for a bit

--- IceBeats.ino
+++ IceBeats.ino
@@ -52,13 +52,18 @@ const int KICK_MAX_SIZE_SECTION = STRIP_LENGTH / 8;
  /**********************
   * HARDWARE VARIABLES *
   *********************/
+
+//Audio setup: Uses ADC for input, then uses the magic of software for the following steps:
+// ADC goes into amp, amp output goes into objects to detect audio peaks and FFT analysis
   
 // GUItool: begin automatically generated code
-AudioInputAnalog         adc1;           //xy=274,198
-AudioAnalyzePeak         peak1;          //xy=431,178
+AudioInputAnalog         adc;           //xy=274,198
+AudioAmplifier           amp;           //xy=333,187
+AudioAnalyzePeak         peak;          //xy=431,178
 AudioAnalyzeFFT1024       fft;       //xy=436,222
-AudioConnection          patchCord1(adc1, fft);
-AudioConnection          patchCord2(adc1, peak1);
+AudioConnection          patchCord1(adc, amp);
+AudioConnection          patchCord2(amp, fft);
+AudioConnection          patchCord3(amp, peak);
 // GUItool: end automatically generated code
 
 CRGB leds[STRIP_LENGTH];
@@ -67,6 +72,8 @@ Bounce debug0 = Bounce(5, 5); //DEBUG 0: Swap palette
 Bounce debug1 = Bounce(4, 5); //DEBUG 1: Force bass kick/Swap visualization effect
 Bounce debug2 = Bounce(3, 5); //DEBUG 2: Force getBassKickProgress() (can also force all FFT section reads, uncomment below)
 
+RunningAverage averagePeak(10); //The average measured peak, used for AGC
+float curGain = 1; //Current gain to use, used for AGC
 
 
 /***************************
@@ -146,6 +153,7 @@ void setup() {
   bassPeakAverage.clear(); //Clear out running averages for the bass
   shortBassAverage.clear();
   bassPeakAverage.addValue(0); //And add some initial data to the peak average
+  averagePeak.clear();
 }
 
 
@@ -164,10 +172,12 @@ void loop() {
 
   curMillis = millis();
   
-  if (peak1.available()) { //We have peak data to read! READ IT!
-    float peak = peak1.read();
+  if (peak.available()) { //We have peak data to read! READ IT!
+    float curPeak = peak.read();
     //Serial.println(peak);
-    idling = (peak <= 0.05);
+    
+    //First, determine if we should be starting an idle animation (audio has been quiet for a while)
+    idling = (curPeak <= 0.05);
     if (idling == true && prevIdling == false) { //We just got a f a l l i n g  i d l i n g  e d g e (wat)
       nextIdleStartMillis = curMillis + 10000; //Start an idle anim in 10 seconds
     }
@@ -178,9 +188,26 @@ void loop() {
       }
     }
     prevIdling = idling;
+
+
+    //Now let's handle AGC - Theorhetically
+    averagePeak.addValue(curPeak);
+    
+    if (averagePeak.getAverage() > 0.75) { //LOUD MUSIC
+      curGain *= 0.98; //Reduce gain, update the amp!
+      amp.gain(curGain);
+      
+    } else if (averagePeak.getAverage() < 0.75 && curGain <= 4) { //we quiet bois and we haven't gotten HYPER LOUD yet
+      curGain *= 1.001; //Slightly increase gain, update amp
+      amp.gain(curGain);
+    }
+    /*Serial.print(curPeak);
+    Serial.print(" ");
+    Serial.println(curGain);*/
+    
     
     if (curEffect == VEDebugFFT) { //Debugging the FFT? Map the peak to the first pixel!
-      leds[0] = CHSV(0, 0, peak * 255);
+      leds[0] = CHSV(0, 0, curPeak * 255);
     }
   }
 
@@ -252,10 +279,14 @@ bool getBassKicked() {
 
   if (bassChangeIncreasing && curBassChange < lastBassChange) { //We were going up but are now going down, record a peak
     bassChangeIncreasing = false;
-    if (lastBassChange / curBassPeakAverage >= 0.8 || lastBassChange >= curBassPeakAverage) { //Our new bass peak should be either greater than or at least *mostly* close to the current peak average (within 80%) to get rid of some super tiny unimportant bass spikes
+    if (lastBassChange >= curBassPeakAverage * 0.85) { //Our new bass peak should be either greater than or at least *mostly* close to the current peak average (within 80%) to get rid of some super tiny unimportant bass spikes
       bassPeakAverage.addValue(lastBassChange);
       bassKicked = true;
+      
+    } else if (curMillis >= lastBassKickMillis + 650) { //No bass kicks in a while, our peak average is probably too high, so let's try lowering it
+      bassPeakAverage.addValue(curBassPeakAverage * 0.8);
     }
+    
   } else if (!bassChangeIncreasing && curBassChange > lastBassChange) { //We were going down but are now going up, how fun
     bassChangeIncreasing = true;
   }
@@ -263,11 +294,14 @@ bool getBassKicked() {
   lastBassChange = curBassChange; //Now store the last bass change and average for the next read
   lastShortBassAverage = curBassValue;
   
-  Serial.print(curBassValue * 3);
+  Serial.print(curBassValue * 3); //DEBUG: Print the current bass bin's value
   Serial.print(" ");
-  Serial.print(curBassPeakAverage * 20);
+  Serial.print(curBassPeakAverage * 20); //The average bass peak value
   Serial.print(" ");
-  Serial.println(curBassChange * 20);
+  Serial.print(curBassPeakAverage * 0.85 * 20); //The threshold to trigger a bass kick (average peak * 0.85)
+  Serial.print(" ");
+  Serial.print(curBassChange * 20); //And the current amount the bass has changed this update
+  Serial.print(" ");
   /*Serial.print(" ");
   Serial.println(abs((curBassValue - curBassAverage)));*/
   //if (((curBassValue >= curBassAverage * 1.3 && curBassValue >= 0.15) || debug1.rose()) && curMillis >= lastBassKickMillis + 300) { //If the bass section just increased a ton (over a certain amount) or the debug button was pressed, return TRUE if we've also had a certain amount of time pass since the last kick
@@ -275,8 +309,10 @@ bool getBassKicked() {
   //if (abs((curBassValue / curBassPeakAverage)) >= 1.4  && abs((curBassValue - curBassPeakAverage)) >= 0.15 && curMillis >= lastBassKickMillis + 300) { //The bass section is above the average, is above a certain "volume" threshold, and some time has passed since the last kick
   if (bassKicked && curMillis >= lastBassKickMillis + 200) { //We detected a new peak earlier and it's been a bit since a bass kick
     lastBassKickMillis = curMillis;
+    Serial.println("6");
     return true;
   }
+  Serial.println("0");
   return false;
 }
 
@@ -315,13 +351,13 @@ void setNewPalette(int paletteId) {
   memcpy(curPalette, palettes[curPaletteIndex], sizeof(curPalette)); //Copy the contents of the new palette to use to curPalette[]
   
   for (short i = 0; i <= 3; i++) { //Also set the current dim palette's values to be a dim version of the current palette
-    curPaletteDim[i] = CHSV(curPalette[i].h, curPalette[i].s, curPalette[i].v * 0.7);
+    curPaletteDim[i] = CHSV(curPalette[i].h, curPalette[i].s, curPalette[i].v * 0.6);
   }
 
   if (curEffect == VEPulse) { //Visualizing pulse or punch, also set the color to use for bass kicks
-    bass_scroll_color = CHSV(curPalette[0].h + 15, constrain(curPalette[0].s - 40, 0, 255), 160);
+    bass_scroll_color = CHSV(curPalette[0].h + 15, constrain(curPalette[0].s - 40, 0, 255), 130);
   } else if (curEffect == VEPunch) {
-    bass_scroll_color = CHSV(curPalette[0].h + 10, constrain(curPalette[0].s - 40, 0, 255), 120);
+    bass_scroll_color = CHSV(curPalette[0].h + 10, constrain(curPalette[0].s - 40, 0, 255), 100);
   }
 }
 
@@ -530,7 +566,7 @@ void visualizeKick() {
 
   //Is it time to update the scroll yet?
   if (curMillis >= nextScrollUpdateMillis) {
-    nextScrollUpdateMillis = curMillis + (getBassKickProgress() > 0 ? 5 : 150); //Find the next time to update the scroll at - don't wait as long if the bass isn't kicking
+    nextScrollUpdateMillis = curMillis + (getBassKickProgress() > 0 ? 2 : 175); //Find the next time to update the scroll at - don't wait as long if the bass isn't kicking
     //avgBassSize = 0;
     scrollOffset++;
     if (scrollOffset > sectionSize[sectionOffset]) { //Scrolled an entire section's worth of pixels, now offset the section instead and reset the scroll
